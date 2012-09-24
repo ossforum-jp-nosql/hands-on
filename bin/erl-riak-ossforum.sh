a23467edb1301b12f87a8e574f7c956df48537b8
Tatsuay Kawano <tatsuya6502@gmail.com>
2012-09-25 06:00:27 +0900

    Add rlwrap.

--- bin/erl-riak-ossforum.sh
+++ bin/erl-riak-ossforum.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # -*- mode:shell-script; coding:utf-8 -*-
 
-erl +A 4 -pa $HOME/riak-erlang-client/ebin -pa $HOME/riak-erlang-client/deps/*/ebin
+rlwrap -adummy erl +A 4 -pa $HOME/riak-erlang-client/ebin -pa $HOME/riak-erlang-client/deps/*/ebin
